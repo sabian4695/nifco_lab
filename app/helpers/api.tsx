14cf27c4a635571365ce8821c620a3530cf6fe36
Jacob Brown <jbrow4@gmail.com>
2025-09-04 08:59:31 -0400

    cleanup, add pg

---
+++ app/helpers/api.tsx
@@ -0,0 +1,12 @@
+require('dotenv').config();
+//@ts-ignore
+import { Pool } from 'pg'
+ 
+const pool = new Pool()
+
+
+const resp = await pool.query('SELECT NOW() as now')
+
+console.log(resp)
+
+export default resp
