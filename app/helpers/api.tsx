79af4ef569af2b8eeb2aeda7848347be0e65379a
Jacob Brown <jbrow4@gmail.com>
2025-09-04 09:16:15 -0400

    updates

--- app/helpers/api.tsx
+++ app/helpers/api.tsx
@@ -1,9 +1,10 @@
-require('dotenv').config();
+import dotenv from 'dotenv' 
+dotenv.config()
+
 //@ts-ignore
 import { Pool } from 'pg'
  
 const pool = new Pool()
-
 
 const resp = await pool.query('SELECT NOW() as now')
 
