b3ed1f08e44c27e2b867d644952c2f897e124bbf
Jacob <113521203+sabian4695@users.noreply.github.com>
2025-09-26 14:02:23 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,17 +7,12 @@ export default function Home() {
   const testConnection = () => {
     
   }
-
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
+            Get started by editing
           </li>
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
